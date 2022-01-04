4bec9a049fe0978a5bf9a2e3ce148c621cfa0ece
Jeet Bafna <jeetanil97@gmail.com>
2022-01-04 16:18:04 -0600

    Building test cases for deploying a  contract

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,1 +1,22 @@
 // contract test code will go here
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+
+let accounts;
+
+beforeEach(async () => {
+	// Get a list of all accounts
+	accounts = await web3.eth.getAccounts();
+
+	//use one of the above accounts to deploy the contract
+
+});
+
+
+describe('Inbox', () => {
+	it('deploys a contract', () => {
+		console.log(accounts);
+	})
+})
