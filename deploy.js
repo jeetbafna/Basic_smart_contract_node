251c6201ac55e27ebf45717928dbb7566d0d58b8
Jeet Bafna <jeetanil97@gmail.com>
2022-01-15 18:17:23 -0600

    Creating a deploy script for deploying on real network

--- deploy.js
+++ deploy.js
@@ -1,1 +1,10 @@
-// deploy code will go here
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+	'pluck gate key cat volcano upper round tumble hollow word arctic orbit',
+	'https://rinkeby.infura.io/v3/1c6afdfe23704757b6d25c4ea93cbc08'
+);
+
+const web3 = new Web3(provider);
